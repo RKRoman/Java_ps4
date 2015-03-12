c295ec7a261cd117812e7c78e34eb9ea7c6ce7f7
RKRoman <Roma1.com@mail.ru>
2015-03-12 21:00:45 +0500

    ok

--- Lab1_1/src/lab1_1/WrongAgeValueException.java
+++
@@ -1,6 +0,0 @@
-package lab1_1;
-
-public class WrongAgeValueException extends Exception{
-	WrongAgeValueException(){super("Age of the person can not be negative and zero");}
-
-}
--- lab1_2/src/lab1/Exception.java
+++
@@ -1,5 +0,0 @@
-package lab1;
-
-public class Exception extends ArithmeticException{
-	Exception(String msg){super(msg);};
-}
